0dc5f73bbb609909792bb2cb540cdb79301d2283
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2025-05-11 11:15:01 +0300

    added new assets

---
+++ Shop All - Brands Page/Server/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const connectWithRetry = async () => {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    maxPoolSize: 10,
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, options)
+    console.log("Connected to MongoDB")
+  } catch (err) {
+    console.error("MongoDB connection error:", err)
+    console.log("Retrying connection in 5 seconds...")
+    setTimeout(connectWithRetry, 5000)
+  }
+}
+
+module.exports = connectWithRetry;
